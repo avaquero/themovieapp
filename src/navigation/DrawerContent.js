82dc3c91050510996180ba97f6181b7e96a481a9
Adrián Vaquero <avaquero1469@gmail.com>
2021-01-12 12:50:52 +0100

    clase 112 y 113

--- src/navigation/DrawerContent.js
+++ src/navigation/DrawerContent.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { DrawerContentScrollView } from "@react-navigation/drawer";
 import { Drawer, Switch, TouchableRipple, Text } from "react-native-paper";
+import usePreferences from "../hooks/usePreferences";
 
 export default function DrawerContent(props) {
     const { navigation } = props;
     const [active, setActive] = useState("home");
+    const { theme, toggleTheme } = usePreferences();
+
+
 
     const onChangeScreen = (screen) => {
         setActive(screen);
@@ -32,8 +36,25 @@ export default function DrawerContent(props) {
                 />
 
             </Drawer.Section>
+            <Drawer.Section title="Opciones">
+                <TouchableRipple>
+                    <View style={styles.preferences}>
+                        <Text>Tema Oscuro</Text>
+                        <Switch value={theme === "dark"} onValueChange={toggleTheme} />
+                    </View>
+                </TouchableRipple>
+            </Drawer.Section>
         </DrawerContentScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    preferences: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+
+    },
+});
